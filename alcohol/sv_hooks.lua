6e6b1a54aaca060081c9fcacb8a338c264e8ebd9
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2022-10-18 16:01:55 +0300

    Update sv_hooks.lua

--- alcohol/sv_hooks.lua
+++ alcohol/sv_hooks.lua
@@ -1,19 +1,13 @@
 local PLUGIN = PLUGIN or {}
 
 function PLUGIN:PlayerDeath(client)
-    local uniqueID = "ixAlcohol" .. client:UniqueID()
-    timer.Remove(uniqueID)
-    client:SetLocalVar("drunk", 0)
-end
-
-function PLUGIN:PlayerLoadedCharacter(client)
-    local uniqueID = "ixAlcohol" .. client:UniqueID()
-    timer.Remove(uniqueID)
-    client:SetLocalVar("drunk", 0)
+    client:DeleteDrunkEffect()
 end
 
 function PLUGIN:PlayerDisconnected(client)
-    local uniqueID = "ixAlcohol" .. client:UniqueID()
-    timer.Remove(uniqueID)
-    client:SetLocalVar("drunk", 0)
+    client:DeleteDrunkEffect()
+end
+
+function PLUGIN:PlayerLoadedCharacter(client)
+    client:DeleteDrunkEffect()
 end
